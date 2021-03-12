a6aea69167e9bf82303d44c0714a9b4dbca1cf57
Bao <zzbaodo@gmail.com>
2021-03-12 00:21:02 -0800

    ready for deployment

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express")
-const {graphqlHTTP} = require("express-graphql")
-const schema = require('./schema')
-
-
+const { graphqlHTTP } = require("express-graphql")
+const cors = require("cors")
+const schema = require("./schema")
+const path = require("path")
 const app = express()
+
+const PORT = process.env.PORT || 5000
+app.use(cors())
 
 app.use(
   "/graphql",
@@ -13,6 +16,8 @@ app.use(
   })
 )
 
-const PORT = process.env.PORT || 5000
+if ((process.env.NODE_ENV = "production")) {
+  app.use(express.static("client/build"))
+}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
